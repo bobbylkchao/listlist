c64cb605d16b186de6a489f535f7080c679257c9
Bobby Chao <46798524+bobbylkchao@users.noreply.github.com>
2021-11-17 15:08:32 -0600

    Update: modify posts api, prepare for category list page

--- api/resolvers/Query/posts.js
+++ api/resolvers/Query/posts.js
@@ -1,4 +1,4 @@
-const { GraphQLList, GraphQLInt, GraphQLNonNull } = require('graphql');
+const { GraphQLList, GraphQLInt, GraphQLNonNull, GraphQLString } = require('graphql');
 const { dbQuery } = require('../../database');
 const { PostType } = require('../../types');
 
@@ -6,19 +6,72 @@ const queryPosts = {
   type: new GraphQLList(PostType),
   description: "Get post list.",
   args: {
-    categoryID: {
-      type: new GraphQLNonNull(GraphQLInt),
-      description: "The id of the category that needs to get the data."
+    country: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "Country of posts, eg: CA"
     },
-    lastID: {
+    region: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "Region of posts, eg: MB"
+    },
+    city: {
+      type: GraphQLString,
+      description: "(Optional) City of posts, eg: WINNIPEG"
+    },
+    categoryID: {
       type: GraphQLInt,
-      description: "(Optional) From which ID to get the data."
+      description: "(Optional) The id of the category that needs to get the data."
+    },
+    topID: {
+      type: GraphQLInt,
+      description: "(Optional) From which ID to get the data, used to get latest data"
+    },
+    bottomID: {
+      type: GraphQLInt,
+      description: "(Optional) From which ID to get the data, used to get previous data"
     },
   },
-  async resolve(_, { categoryID, lastID }){
-    let fromIDFilter = lastID ? `AND  id < ${lastID}` : '';
-    let categoryIDFilter = categoryID ? `WHERE categoryID = ${categoryID}` : '';
-    let res = await dbQuery(`SELECT * FROM Post ${categoryIDFilter} ${fromIDFilter} ORDER BY id desc, updatedAt desc limit 10`);
+  async resolve(_, { country, region, city, categoryID, topID, bottomID }){
+    //let categoryIDFilter = categoryID ? `WHERE categoryID = ${categoryID}` : '';
+    //let fromIDFilter = lastID ? `AND  id < ${lastID}` : '';
+    //let res = await dbQuery(`SELECT * FROM Post ${categoryIDFilter} ${fromIDFilter} ORDER BY id desc, updatedAt desc limit 10`);
+    const dataCountPerTime = 10;
+    let res = "";
+    let sql = "";
+    let sql_part_main = "SELECT * FROM Post WHERE country = ? AND region = ?";
+    let sql_part_orderby = "ORDER BY id desc, updatedAt desc";
+    let sql_limit = `LIMIT ${dataCountPerTime}`;
+    let params = [country, region];
+
+    if(city){
+      // SELECT * FROM Post WHERE country = ? AND region = ? AND city = ? ORDER BY id desc, updatedAt desc limit 10
+      sql_part_main = `${sql_part_main} AND city = ?`;
+      params = [...params, city];
+    }
+
+    if(categoryID){
+      // SELECT * FROM Post WHERE country = ? AND region = ? AND categoryID = ? ORDER BY id desc, updatedAt desc limit 10
+      sql_part_main = `${sql_part_main} AND categoryID = ?`;
+      params = [...params, categoryID];
+    }
+
+    if(topID){
+      // SELECT * FROM Post WHERE country = ? AND region = ? AND id > ? ORDER BY id desc, updatedAt desc
+      sql_limit = "";
+      sql_part_main = `${sql_part_main} AND id > ?`;
+      params = [...params, topID];
+    }
+
+    if(bottomID){
+      // SELECT * FROM Post WHERE country = ? AND region = ? AND id < ? ORDER BY id desc, updatedAt desc limit 10
+      sql_part_main = `${sql_part_main} AND id < ?`;
+      params = [...params, bottomID];
+    }
+
+    sql = `${sql_part_main} ${sql_part_orderby} ${sql_limit}`;
+
+    res = await dbQuery(sql, params);
+    
     return res;
   }
 };
