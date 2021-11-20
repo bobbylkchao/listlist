e6b30f936f08b53776b64998af740d677e3cc73e
Bobby Chao <46798524+bobbylkchao@users.noreply.github.com>
2021-11-19 21:11:32 -0600

    Delete db.config.js

--- api/config/db.config.js
+++
@@ -1,8 +0,0 @@
-// Database Configuration
-module.exports = db = {
-  "DB_HOST": "127.0.0.1",
-  "DB_USER": "root",
-  "DB_PASSWORD": "bos999!!!",
-  "DB_PORT": 3306,
-  "DB_DATABASE": "listlist"
-};
